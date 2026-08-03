26558214965c5b9ed0dbc0c82e198dd3482d2649
Mányik Bence <h161847@stud.u-szeged.hu>
2026-08-03 18:31:56 +0200

    Fix create function

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,5 +3,10 @@ INSERT INTO parking_spots (id, spot_number, spot_type, description) VALUES (2, '
 INSERT INTO parking_spots (id, spot_number, spot_type, description) VALUES (3, 'EV-201', 'ELECTRIC_CHARGING', 'Spot with 22kW EV charger');
 INSERT INTO parking_spots (id, spot_number, spot_type, description) VALUES (4, 'DISABLED-01', 'HANDICAPPED', 'Reserved for disabled badge holders');
 
-INSERT INTO reservations (id, parking_spot_id, applicant_name, start_time, end_time, cancelled)
-VALUES (1, 1, 'Kovács Péter', '2026-08-10 08:00:00', '2026-08-10 16:00:00', false);
+-- A kezdő foglalás beszúrása explicit ID nélkül:
+INSERT INTO reservations (parking_spot_id, applicant_name, start_time, end_time, cancelled)
+VALUES (1, 'Kovács Péter', '2026-08-10 08:00:00', '2026-08-10 16:00:00', false);
+
+-- A sequence számlálók igazítása az inicializált adatokhoz:
+SELECT setval('parking_spots_id_seq', (SELECT MAX(id) FROM parking_spots));
+SELECT setval('reservations_id_seq', (SELECT MAX(id) FROM reservations));
